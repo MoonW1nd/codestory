c460e7743150fb55d0d0038a85042252cc0dcb0d
Alexander Ivankov <moonw1nd@yandex-team.ru>
2020-07-05 18:49:09 +0300

    feat: add package for work with git log

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import clear from 'clear';
+import figlet from 'figlet';
+import yargs from 'yargs';
+import {gitlogPromise, GitlogOptions} from 'gitlog';
+
+clear();
+console.log(chalk.red(figlet.textSync('code story', {horizontalLayout: 'full'})));
+
+const argv = yargs.options({
+    from: {type: 'string', default: false},
+}).argv;
+
+const options: GitlogOptions<'authorDate' | 'subject'> = {
+    repo: __dirname,
+    number: 20,
+    author: 'Alexander Ivankov',
+    fields: ['authorDate', 'subject'],
+    execOptions: {maxBuffer: 1000 * 1024},
+};
+
+gitlogPromise(options).then((commits) => console.log(commits));
