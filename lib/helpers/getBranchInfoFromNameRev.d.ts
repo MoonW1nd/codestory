28abc040c9a042d4e483a873ad50d9dbae6f5e1b
Alexander Ivankov <moonw1nd@yandex-team.ru>
2020-08-31 09:13:51 +0300

    chore: remove build from source

--- lib/helpers/getBranchInfoFromNameRev.d.ts
+++
@@ -1,9 +0,0 @@
-export interface BranchInfo {
-    name: string;
-    position: number;
-    remote?: string;
-}
-/**
- * Get breach info from stdout git nave-rev command
- */
-export declare const getBranchInfoFromNameRev: (nameRev: string) => BranchInfo;
