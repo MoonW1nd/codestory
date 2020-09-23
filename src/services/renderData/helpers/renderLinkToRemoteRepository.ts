820dd1e8f01527c0b247f5a4c4873f58ef51a719
Alexander Ivankov <alexanderivankoff@gmail.com>
2020-09-23 23:35:59 +0300

    refactor(render): add helper renderLinkToRemoteRepository

---
+++ src/services/renderData/helpers/renderLinkToRemoteRepository.ts
@@ -0,0 +1,26 @@
+import {Branch} from '@project-types/entities';
+import {TextEntity} from 'src/constants';
+import chalkTextEntity from 'src/services/renderData/helpers/chalkEntity';
+import renderLineWithLabel from 'src/services/renderData/helpers/renderLineWithTitle';
+
+const renderLinkToRemoteRepository = (branch: Branch): void => {
+    const {repositoryUrl, refType, id: name} = branch;
+
+    if (repositoryUrl) {
+        const chalkedUrl = chalkTextEntity(TextEntity.url, repositoryUrl);
+
+        let label: string;
+
+        if (name === 'master') {
+            label = 'branch';
+        } else if (refType === 'tag') {
+            label = 'tag';
+        } else {
+            label = 'pr';
+        }
+
+        renderLineWithLabel(label, chalkedUrl);
+    }
+};
+
+export default renderLinkToRemoteRepository;
